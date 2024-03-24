5908e29946c7341ee82c0f9287b5fe76a038b532
CameronDudd <110551761+CameronDudd@users.noreply.github.com>
2024-03-24 23:38:26 +0000

    add include/*

---
+++ include/collections.h
@@ -0,0 +1,19 @@
+// Copyright 2024 Cameron Dudd
+
+#ifndef COLLECTIONS_H
+#define COLLECTIONS_H
+
+typedef struct {
+  char *list;
+  int   used;
+  int   size;
+} List;
+
+extern void initList(List *l, int initSize);
+extern void appendList(List *l, char element);
+extern void listcpy(List *src, List *dst);
+extern void printList(List *l);
+extern void freeList(List *l);
+extern int  testCollections();
+
+#endif /* COLLECTIONS_H */
---
+++ include/fileio.h
@@ -0,0 +1,11 @@
+// Copyright 2024 Cameron Dudd
+
+#ifndef FILEIO_H
+#define FILEIO_H
+
+#include "./collections.h"
+
+extern void readlines(char *filename, List linsbuffer);
+extern void testFileIO();
+
+#endif /* FILEIO_H */
