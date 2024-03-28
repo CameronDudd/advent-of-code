0ae5b1b6239b233fdf613021bebcff215a787a78
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2024-03-28 01:20:36 +0000

    add math

---
+++ include/math.h
@@ -0,0 +1,3 @@
+// Copyright 2024 Cameron Dudd
+
+int min(int a, int b);
---
+++ src/math.c
@@ -0,0 +1,9 @@
+// Copyright 2024 Cameron Dudd
+
+int min(int a, int b) {
+  // tiebreaker returns first arg
+  if (a <= b) {
+    return a;
+  }
+  return b;
+}
